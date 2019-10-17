195ad6f5ae191a2ae5f0b82f5c69d4a1f7867c9d
uqjwen <wen_jiahui@outlook.com>
2019-10-17 21:58:16 +0800

    wenjh

--- process.py
+++ process.py
@@ -1,3 +1,4 @@
+import pickle
 import numpy as np 
 
 def get_ui():
@@ -7,11 +8,11 @@ def get_ui():
 	# for line in fr.readlines()
 	data = fr.readlines()
 	fr.close()
-	uir = []
+	uirs = []
 	for line in data:
 		line = line.strip()
 		listfromline = line.split()
-		uir.append(listfromline)
+		uirs.append(listfromline)
 		user = listfromline[0]
 		item = listfromline[1]
 		if user not in u_dict:
@@ -26,13 +27,47 @@ def get_ui():
 
 	u_min = 5
 	i_min = 5
-
+	print(len(u_dict), len(i_dict))
 	flag = 0
 	while flag == 0:
 		flag = 1
 		for user in list(u_dict.keys()):
-			if len()
+			if len(u_dict[user])<u_min:
+				for item in u_dict[user]:
+					i_dict[item].remove(user)
+				del u_dict[user]
+				flag = 0
+		for item in list(i_dict.keys()):
+			if len(i_dict[item])<i_min:
+				for user in i_dict[item]:
+					u_dict[user].remove(item)
+				del i_dict[item]
+				flag = 0
+
+
+	user2idx = {user:i for i,user in enumerate(u_dict.keys())}
+	item2idx = {item:i for i,item in enumerate(i_dict.keys())}
+
+	uir = []
+	for record in uirs:
+		user = record[0]
+		item = record[1]
+		rate = record[2]
+		if user not in user2idx or item not in item2idx:
+			continue
+		uir.append([user2idx[user], item2idx[item], rate])
+
+	data = {}
+	data['uir'] = uir 
+	data['num_user'] = len(user2idx)
+	data['num_item'] = len(item2idx)
+
+
+	print(len(uir), len(user2idx), len(item2idx))
+	fr = open('temp_data.pkl', 'wb')
+	pickle.dump(data, fr)
+	fr.close()
 
 
 if __name__ == '__main__':
-	main()
+	get_ui()
